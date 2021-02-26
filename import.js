e78ded9d7f803d263c3eabb0a243f36177b36a14
Erwan BOEHM <erwan.boehm@gmail.com>
2021-02-26 12:43:59 +0100

    first commit

---
+++ import.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const mammoth = require('mammoth');
+const fs = require('fs');
+const settings = require('ep_etherpad-lite/node/utils/Settings');
+
+exports.import = (hookName, args, callback) => {
+  const srcFile = args.srcFile;
+  const destFile = args.destFile;
+
+  if (!settings.ep_docx) {
+    settings.ep_docx = {};
+  }
+  if (settings.ep_docx.ignoreEmptyParagraphs !== false) {
+    settings.ep_docx.ignoreEmptyParagraphs = true;
+  }
+
+  const options = {
+    styleMap: [
+      "p[style-name='center'] => p:fresh > center",
+      "p[style-name='right'] => p:fresh > right",
+      "p[style-name='left'] => p:fresh > left",
+      "p[style-name='justify'] => p:fresh > justify",
+
+      "p[style-name='Heading 1'] => p:fresh > h1:fresh",
+      "p[style-name='Heading 2'] => p:fresh > h2:fresh",
+      "p[style-name='Heading 3'] => p:fresh > h3:fresh",
+      "p[style-name='Heading 4'] => p:fresh > h4:fresh",
+      "p[style-name='Heading 5'] => p:fresh > h5:fresh",
+      "p[style-name='Heading 6'] => p:fresh > h6:fresh",
+
+      // CUSTOM ERWAN GOOGLE DOCS
+      "Title => p:test"
+    ],
+    transformDocument: transformElement,
+    ignoreEmptyParagraphs: settings.ep_docx.ignoreEmptyParagraphs,
+  };
+
+  // First things first do we handle this doc type?
+  const docType = srcFile.split('.').pop();
+
+  if (docType !== 'docx') return callback(); // we don't support this doctype in this plugin
+  console.log('Using mammoth[ep_docx] to convert DocX file');
+
+  mammoth.convertToHtml(
+      {
+        path: srcFile,
+      }, options).then(
+      (result) => {
+        console.log("RESULT : ", result);
+        fs.writeFile(destFile, `<!doctype html>\n<html lang='en'>
+            <body>
+            ${result.value}
+            </body>
+            </html>
+          `, 'utf8', (err) => {
+          if (err) callback(err, null);
+          callback(destFile);
+        });
+      })
+      .fail((e) => {
+        console.warn('Mammoth failed to import this file');
+        return callback();
+      })
+      .done(() => {
+
+      });
+};
+
+const transformElement = (element) => {
+  if (element.children) {
+    element.children.forEach(transformElement);
+  }
+  if (element.type === 'paragraph') {
+    if (element.alignment === 'center' && !element.styleId) {
+      element.styleName = 'center';
+    }
+    if (element.alignment === 'left' && !element.styleId) {
+      element.styleName = 'left';
+    }
+    if (element.alignment === 'right' && !element.styleId) {
+      element.styleName = 'right';
+    }
+    if (element.alignment === 'justify' && !element.styleId) {
+      element.styleName = 'justify';
+    }
+  }
+  return element;
+};
